5557c3189fb548188027f3d8f6bf198c62fb9b1b
Ha Le <haqle322@gmail.com>
2016-10-19 15:54:18 -0500

    Added solution for 'Translating RNA into Protein'

--- rna2protein.cpp
+++ rna2protein.cpp
@@ -11,22 +11,33 @@ typedef unordered_map<string, char> umap;
 umap get_rna_code(string table){
     //Let's agree that 'Z' represents Stop codon
     umap out;
-    fstream rna_code;
+    fstream rna_code("rna_codon_table.txt");
     string line;
-    rna_code.open(table);
-    getline(rna_code, line);
-    regex codon_rgx("((([AUGC]{3})[[:space:]]([A-Z])[[:space:]]*){4})");
-    smatch codon;
-    do {
-        if (regex_search(line, codon, codon_rgx)) {
-            cout << codon[0] << endl;//"<1\t2>" << codon[1] << "\t" << codon[2] << endl;
-            for (auto res : codon) cout << res << endl;
-        }
+    regex codon("(([AUGC]{3})\\s([A-Z]))");
+    while (!rna_code.eof()) {
         getline(rna_code, line);
-    } while (!rna_code.eof());
+        auto line_begin = std::sregex_iterator(line.begin(), line.end(), codon);
+        auto line_end = std::sregex_iterator();
+        for (std::sregex_iterator i = line_begin; i != line_end; ++i) {
+            std::smatch match = *i;
+            out.insert(make_pair(match[2].str(), match[3].str()[0]));
+        }
+    }
     return out;
 }
 int main() {
-    get_rna_code("rna_codon_table.txt");
+    umap protein_table = get_rna_code("rna_codon_table.txt");
+    fstream rna_file("rna2protein.txt");
+    string rna_seq, codon;
+    string protein = "";
+    getline(rna_file, rna_seq);
+    for (int i = 0; i < rna_seq.size() - 2; i+=3) {
+        codon = rna_seq.substr(i,3);
+        if (protein_table[codon] == 'Z') {
+            break;
+        }
+        protein += protein_table[codon];
+    }
+    cout << protein << endl;
     return 0;
 }
